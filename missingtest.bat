bdca4d8d6ee49f86b9fd5d70f94180a02818f584
Jeremy Corson <Jeremy.r.corson@gmail.com>
2013-03-06 15:42:39 -0500

    More updates. jar and updated the bat

--- missingtest.bat
+++ missingtest.bat
@@ -1,8 +1,8 @@
 echo eSalesCommonClient
-java -cp bin lmig.util.LmigUtil missingtests "D:\Workspaces\PLM\PmESalesCommonClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesCommonClient\src\main\webapp\test\unit" "js" test\,.profile
+java -jar dist\LmigUtil.jar missingtests "D:\Workspaces\PLM\PmESalesCommonClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesCommonClient\src\main\webapp\test\unit" "js" test\,.profile
 
 echo eSalesDesktopCommonClient
-java -cp bin lmig.util.LmigUtil missingtests "D:\Workspaces\PLM\PmESalesDesktopCommonClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesDesktopCommonClient\src\main\webapp\test\unit" "js" test\,.profile
+java -jar dist\LmigUtil.jar missingtests "D:\Workspaces\PLM\PmESalesDesktopCommonClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesDesktopCommonClient\src\main\webapp\test\unit" "js" test\,.profile
 
 echo eSalesDesktopAutoClient
-java -cp bin lmig.util.LmigUtil missingtests "D:\Workspaces\PLM\PmESalesDesktopAutoClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesDesktopAutoClient\src\main\webapp\test\unit" "js" test\,.profile
+java -jar dist\LmigUtil.jar missingtests "D:\Workspaces\PLM\PmESalesDesktopAutoClient\src\main\webapp" "D:\Workspaces\PLM\PmESalesDesktopAutoClient\src\main\webapp\test\unit" "js" test\,.profile
